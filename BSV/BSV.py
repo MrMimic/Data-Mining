44ec72c6a027980bd2d6580a7b2b4258a27ee616
MrMimic <emeric.dynomant@omictools.com>
2018-01-20 09:52:14 +0100

    Advance in algorithm description

--- BSV/BSV.py
+++ BSV/BSV.py
@@ -1,76 +1,71 @@
 #!/usr/bin/env python3
 
 
-
 import os
 import re
 import nltk
 import math
 import json
 import string
+import pandas as pd
 import configparser
 from lxml import etree
 from bs4 import BeautifulSoup
 from collections import Counter
 from nltk.corpus import stopwords
 from textblob import TextBlob as tb
+# sentiment analysis included
+from matplotlib import pyplot as plt
 from nltk.stem import WordNetLemmatizer
 
-
-
+# Mapping termino AFTER tfIDF > acurracy increase
 
 class GenericDataAnalyzer(object):
 
-
 	def __init__(self, configuration):
-		"""
-		Blabla
-		"""
+		"""Generic textual data analyzer"""
 
 		self.config = configparser.ConfigParser()
 		self.config.read(configuration)
-
+		# Get filelist
 		self.base_directory = self.config.get('utilities', 'base_directory')
 		self.file_list = os.listdir(self.base_directory)
-
 		self.passed = 0
-
-		self.stopwords = set(stopwords.words('french'))
+		# Dictionnary and punctuation
+		self.stopwords = set(stopwords.words(self.config.get('utilities', 'corpus_lang')))
 		self.punctuation = set(string.punctuation)
-
-		# DATA
+		# Pattern data
 		with open(self.config.get('utilities', 'french_words'), 'r') as myfile:
 			self.french_words = [word.strip('\n') for word in myfile if len(word) > 2]
 		with open(self.config.get('utilities', 'french_departements'), 'r') as myfile:
 			self.departements = [word.strip('\n').lower() for word in myfile]
 		with open(self.config.get('utilities', 'french_regions'), 'r') as myfile:
 			self.regions = [word.strip('\n').lower() for word in myfile]
-
+		with open('./terminology/terminology_csv.csv', 'r') as my_terminology:
+			termino_tree = pd.read_csv(my_terminology)
+			self.kingdoms = [item.lower() for item in termino_tree['kingdom']]
+			self.species = [item.lower() for item in termino_tree['group']]
+			self.organisms = [item.lower() for item in termino_tree['organism']]
 
 	def tf(self, word, blob):
+		"""Term frequency"""
 		return blob.words.count(word) / len(blob.words)
 
-
 	def n_containing(self, word, bloblist):
+		"""Number of occurence"""
 		return sum(1 for blob in bloblist if word in blob.words)
 
-
 	def idf(self, word, bloblist):
+		"""Inverse document frequency"""
 		return math.log(len(bloblist) / (1 + self.n_containing(word, bloblist)))
 
-
 	def tfidf(self, word, blob, bloblist):
+		"""TFIDF itself"""
 		return self.tf(word, blob) * self.idf(word, bloblist)
 
-
 	def find_ngrams(self, input_list, n):
-
-		""" 
-		Easiest way to get nGrams 
-		"""
-
+		"""Gram a list"""
 		return list(zip(*[input_list[i:] for i in range(int(n))]))
-
 
 	def compute_distance(self, word_1, word_2):
 		""" Compute the Levensthein distance between two words """
@@ -96,11 +91,8 @@ class GenericDataAnalyzer(object):
 
 		return distance_matrix[word_1_length-1][word_2_length-1]
 
-
 	def data_preprocessing(self):
-		"""
-		TEXT PREPROCESSING
-		"""
+		"""TEXT PREPROCESSING"""
 
 		cleaned_data = []
 		lemmatiser = WordNetLemmatizer()
@@ -109,14 +101,13 @@ class GenericDataAnalyzer(object):
 			if re.search('BSV', infile) and re.search('txt', infile):
 				document_details = {}
 				with open(self.base_directory+infile, 'r') as myfile:
-					print('\t- Processing: {}'.format(infile))
+					print('\t- Processing: {}...'.format(infile[:50]))
 					# GET RAW DATA
 					data = myfile.read().replace('\n', '').lower()
 					document_details['raw'] = data
 					# TOKENIZE AND GRAM
-					tokenized_data = nltk.word_tokenize(data)
-					#~ document_details['gram_2'] = [word for word in self.find_ngrams(input_list=tokenized_data, n=2)]
-					#~ document_details['gram_3'] = [word for word in self.find_ngrams(input_list=tokenized_data, n=3)]
+					#~ tokenized_data = nltk.word_tokenize(data)
+					tokenized_data = re.findall(r"\w+(?:[-:]{1,2})?(?:\w+)?(?:[-:]{1,2})?(?:\w+)?", str(data))
 					# CLEAN AND LEMME TOKENS
 					lemmed_data = []
 					for word in tokenized_data:
@@ -126,45 +117,15 @@ class GenericDataAnalyzer(object):
 								if word in self.french_words:
 									lemmed_data.append(lemmatiser.lemmatize(word))
 								else:
-									# IF THIS IS A WORD WITH PREFFIX
-									if re.search('[\’\']', word):
-										prefix_removed = re.findall('[a-z]{1,2}[\’\'](.*)', word)
-										if len(prefix_removed) > 0:
-											if (prefix_removed[0] not in self.stopwords) and (prefix_removed[0] not in self.punctuation):
-												if prefix_removed[0] in self.french_words:
-													lemmed_data.append(prefix_removed[0])
-												# BAD ORTHOGRAPHE
-												else:
-													if self.config.getboolean('options', 'correct_ortho') is True:
-														distance = 10
-														for french_word in self.french_words:
-															new_distance = self.compute_distance(word_1=french_word, word_2=prefix_removed[0])
-															if new_distance < distance:
-																print('\t\tCorrecting [{}]\tFound: [{}]  (d={})'.format(word, french_word, new_distance))
-																distance = new_distance
-																word_corrected = french_word
-														lemmed_data.append(word_corrected)
-													else:
-														continue
-									# OR A COMPOSED WORD SPLITTED ON TWO LINES
-									elif re.search('[\-]', word):
-										corrected_word = re.sub('-', '', word)
-										if (corrected_word not in self.stopwords) and (corrected_word not in self.punctuation):
-												if corrected_word in self.french_words:
-													lemmed_data.append(corrected_word)
-												# BAD ORTHOGRAPHE
-												else:
-													if self.config.getboolean('options', 'correct_ortho') is True:
-														distance = 10
-														for french_word in self.french_words:
-															new_distance = self.compute_distance(word_1=french_word, word_2=corrected_word)
-															if new_distance < distance:
-																print('\t\tCorrecting [{}]\tFound: [{}]  (d={})'.format(word, french_word, new_distance))
-																distance = new_distance
-																word_corrected = french_word
-														lemmed_data.append(word_corrected)
-													else:
-														continue
+									if self.config.getboolean('options', 'correct_ortho') is True:
+										distance = 10
+										for french_word in self.french_words:
+											new_distance = self.compute_distance(word_1=french_word, word_2=prefix_removed[0])
+											if new_distance < distance:
+												print('\t\tCorrecting [{}]\tFound: [{}]  (d={})'.format(word, french_word, new_distance))
+												distance = new_distance
+												word_corrected = french_word
+										lemmed_data.append(word_corrected)
 					# STORE
 					document_details['lemmed_data'] = lemmed_data
 					document_details['file_name'] = infile
@@ -176,11 +137,8 @@ class GenericDataAnalyzer(object):
 
 		return cleaned_data
 
-
 	def train_tfidf(self, data):
-		"""
-		Blabla
-		"""
+		"""Blabla"""
 
 		# JOIN EVERY LEMMED TERMS IN A STR() FOR TEXTBLOB
 		bloblist = []
@@ -191,7 +149,7 @@ class GenericDataAnalyzer(object):
 			scores = {word: self.tfidf(word, bloblist[indice], bloblist) for word in bloblist[indice].words}
 			sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 			data[indice]['scored_words'] = list(sorted_words)
-		print('Model trained and words scored (TF-IDF).\n')
+		print('\n\nModel trained and words scored (TF-IDF).\n')
 		# SAVE OR NOT
 		if self.config.getboolean('options', 'save_model') is True:
 			with open('tfidf_model.json', 'w') as model_file:
@@ -199,84 +157,87 @@ class GenericDataAnalyzer(object):
 
 		return data
 
-
 	def analyze(self, data):
-		"""
-		Blabla
-		"""
+		"""Blabla"""
+
+		# ANALYZE (Steps have to be described in the config with CSV data)
+		data_mapped = []
+		words_found = []
+		if self.config.getboolean('options', 'summarize_data') is True:
+			for document in data:
+				print('\n' + ' ='*20)
+				print('\n{}\t({} words scorred)\n\n\tTop {} words:'.format(document['file_name'], len(document['scored_words']), self.config.getint('options', 'words_per_document')))
+				LINKS = {}
+				for word in document['scored_words'][:self.config.getint('options', 'words_per_document')]:
+					print('\t\t- {}'.format(word))
+					# Keep word for summary plot
+					words_found.append(word[0])
+					# TERMINOLOGY STEP 1: KINGDOM
+					if word[0] in self.kingdoms:
+						try:
+							if 'kingdoms' in LINKS.keys():
+								LINKS['kingdoms'].append(word)
+							else:
+								LINKS['kingdoms'] = [word]
+						except (UnboundLocalError, IndexError):
+							pass
+					# TERMINOLOGY STEP 2: SPECIE
+					if word[0] in self.species:
+						try:
+							if 'species' in LINKS.keys():
+								LINKS['species'].append(word)
+							else:
+								LINKS['species'] = [word]
+						except (UnboundLocalError, IndexError):
+							pass
+					# TERMINOLOGY STEP 3: ORGANISM
+					for organism in self.organisms:
+						if re.search(organism, word[0]):
+							try:
+								if 'organisms' in LINKS.keys():
+									LINKS['organisms'].append(word)
+								else:
+									LINKS['organisms'] = [word]
+							except (UnboundLocalError, IndexError):
+								pass
+					# GEOGRAPHY STEP1: REGIONS
+					if word[0] in self.regions:
+						try:
+							if 'region' in LINKS.keys():
+								LINKS['region'].append(word)
+							else:
+								LINKS['region'] = [word]
+						except (UnboundLocalError, IndexError):
+							pass
+					# GEOGRAPHY STEP1: DEPARTEMENTS
+					if word[0] in self.departements:
+						try:
+							if 'departements' in LINKS.keys():
+								LINKS['departements'].append(word)
+							else:
+								LINKS['departements'] = [word]
+						except (UnboundLocalError, IndexError):
+							pass
+				document['links'] = LINKS
+				data_mapped.append(document)
+				if len(document['links'].keys()) > 0:
+					print('\n\tTerminology mapping:')
+					print(json.dumps(document['links'], indent=2, sort_keys=True, ensure_ascii=False))
 
 		# SUMMARY PLOT
 		if self.config.getboolean('options', 'plot_graphes') is True:
-			pass
-		# THEN, ANALYZE
-		if self.config.getboolean('options', 'summarize_data') is True:
-			for document in data:
-				print('\n{}\t({} words scorred)\n\tTop 10 words:'.format(document['file_name'], len(document['scored_words'])))
-				for word in document['scored_words'][:10]:
-					print('\t\t- {}'.format(word))
+			word_found_counted = Counter(words_found)
+			score_min = self.config.getint('options', 'word_occurences_min')
+			labels = [w[0] for w in word_found_counted.items() if w[1] > score_min]
+			position = range(len(labels))
+			counts = [w[1] for w in word_found_counted.items() if w[1] > score_min]
 
-		# TERMINOLOGY MAPPING
-		print('\nTerminology mapping on you documents.')
-		with open('./terminology/vegetal_french_terminology.xml', 'r') as my_terminology:
-			termino_tree = etree.parse(my_terminology)
-			data_mapped = []
-			kingdoms = [item.text.lower() for item in termino_tree.xpath('/DATA/KINGDOM/ITEM/TERM') if item.text is not None] + [item.text.lower() for item in termino_tree.xpath('/DATA/KINGDOM/ITEM/SYNONYMS/SYN') if item.text is not None]
-			species = [item.text.lower() for item in termino_tree.xpath('/DATA/SPECIE/ITEM/TERM') if item.text is not None]
-			organisms = [item.text.lower() for item in termino_tree.xpath('/DATA/ORGANISM/ITEM/TERM') if item.text is not None]
-
-			# PARSING
-			for document in data:
-				LINKS = {}
-				for word in document['scored_words']:
-					# STEP 1: KINGDOM
-					if word[0] in kingdoms:
-						if word[0] in [item.text.lower() for item in termino_tree.xpath('/DATA/KINGDOM/ITEM/TERM') if item.text is not None]:
-							id_concept = termino_tree.xpath('/DATA/KINGDOM/ITEM[TERM="{}"]/ID'.format(word[0]))
-						###########
-						#~ ### WHAT ABOUT SYNONYMS? HOW TO CLIMB BACK ONE LEVEL TO GET ID FROM A IMBRICADED SYN?
-						#~ elif word[0] in [item.text.lower() for item in termino_tree.xpath('/DATA/KINGDOM/ITEM/SYNONYMS/SYN') if item.text is not None]:
-							#~ t = termino_tree.xpath('/DATA/KINGDOM/ITEM/SYNONYMS[SYN="{}"]'.format(word[0]))
-							#~ print(word[0])
-						##########
-							try:
-								if 'kingdoms' in LINKS.keys():
-									LINKS['kingdoms'].append({id_concept[0].text: word})
-								else:
-									LINKS['kingdoms'] = [{id_concept[0].text: word}]
-							except UnboundLocalError:
-								pass
-					# STEP 2: SPECIE
-					if word[0] in species:
-						id_concept = termino_tree.xpath('/DATA/SPECIE/ITEM[TERM="{}"]/ID'.format(word[0]))
-						try:
-							if 'species' in LINKS.keys():
-								LINKS['species'].append({id_concept[0].text: word})
-							else:
-								LINKS['species'] = [{id_concept[0].text: word}]
-						except UnboundLocalError:
-							pass
-					# STEP 3: ORGANISM
-					if word[0] in organisms:
-						id_concept = termino_tree.xpath('/DATA/ORGANISM/ITEM[TERM="{}"]/ID'.format(word[0]))
-						try:
-							if 'organisms' in LINKS.keys():
-								LINKS['organisms'].append({id_concept[0].text: word})
-							else:
-								LINKS['organisms'] = [{id_concept[0].text: word}]
-						except UnboundLocalError:
-							pass
-
-
-				document['links'] = LINKS
-				data_mapped.append(document)
-
-				if len(document['links'].keys()) > 0:
-					print(document['file_name'])
-					print(json.dumps(document['links'], indent=2, sort_keys=True, ensure_ascii=False))
-					print(' ='*20)
-
-
-
+			figure = plt.figure()
+			subplot = figure.add_subplot(111)
+			subplot.bar(position, counts)
+			subplot.set_xticks(range(len(labels)))
+			subplot.set_xticklabels(labels, rotation=45)
+			plt.show()
 
 
 if __name__ == '__main__':
@@ -289,8 +250,3 @@ if __name__ == '__main__':
 		with open('tfidf_model.json', 'r') as model_saved:
 			scored_data = json.loads(model_saved.read())
 	GDA.analyze(data=scored_data)
-
-
-
-
-
